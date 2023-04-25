994c0f62e07327389c6171e0da7be8e17ef01eeb
Janet Zhang <xiaodan928@gmail.com>
2023-04-26 01:11:18 +0800

    Add files via upload

--- backend/accounting/accounting.py
+++ backend/accounting/accounting.py
@@ -3,7 +3,8 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, allow_headers=['Content-Type', 'Access-Control-Allow-Origin',
+                         'Access-Control-Allow-Headers', 'Access-Control-Allow-Methods'])
 
 class BalanceSheet:
     def __init__(self, year):
